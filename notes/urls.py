f9a491461c548cd517f847cbace691a31d3c6925
Naitik Soni <83502368+Naitik-Soni@users.noreply.github.com>
2023-05-20 15:16:43 +0530

    Update and removed static in urls.py

--- notes/urls.py
+++ notes/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from .views import *
-from django.conf import settings
-from django.conf.urls.static import static
 
 urlpatterns = [
     path("", notes, name="notes"),
     path("addnotes/", addnotes),
     path("updatenote/<int:id>", updatenotes),
     path("delete/<int:id>", delete),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
